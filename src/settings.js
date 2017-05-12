2406eb0aecbdbbca2008bb5c964953a7ab0b7996
Daniel Prado <danielprado.ad@gmail.com>
2017-05-12 19:29:53 -0300

    fix informative details padding bottom

--- src/settings.js
+++ src/settings.js
@@ -12,7 +12,8 @@ const apiTimeout = 15 * 1000;
 const apiEndpoint = env === 'production' ?
   `https://app.icbsorocaba.com.br/api/app/${churchSlug}` :
   // `http://192.168.25.5:3001/api/app/${churchSlug}`;
-  `http://10.84.77.199:3001/api/app/${churchSlug}`;
+  // `http://10.84.77.199:3001/api/app/${churchSlug}`;
+  `https://app.icbsorocaba.com.br/api/app/${churchSlug}`;
 
 
 const googleApi = {
