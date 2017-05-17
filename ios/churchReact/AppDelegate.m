7e868b08d7b694d32cee0b76863f1d3ba6213f72
TVTEM <TVTEM@HSFlaAgo.local>
2017-05-17 00:40:10 -0300

    codepush config

--- ios/churchReact/AppDelegate.m
+++ ios/churchReact/AppDelegate.m
@@ -14,6 +14,7 @@
 #import "SplashScreen.h"
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <RNGoogleSignin/RNGoogleSignin.h>
+#import <CodePush/CodePush.h>
 
 @implementation AppDelegate
 
@@ -21,8 +22,12 @@
 {
   NSURL *jsCodeLocation;
 
+#ifdef DEBUG
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-
+#else
+  jsCodeLocation = [CodePush bundleURL];
+#endif
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"churchReact"
                                                initialProperties:nil
