1a11702fcb22f1676e248f6373c71362604acfee
danieloprado <danielprado.ad@gmail.com>
2017-07-06 10:29:42 -0300

    version 2.2.1

--- release.android.sh
+++ release.android.sh
@@ -1,4 +1,4 @@
-VERSION=2.2.0
+VERSION=2.2.1
 echo "version $VERSION"
 
 rm ICBSorocaba.apk
@@ -17,7 +17,7 @@ react-native bundle \
 bugsnag-sourcemaps upload \
      --api-key 7f394ec621690879be85bc0bef530ac9 \
      --source-map /tmp/icbsorocaba.sourcemap \
-     --app-version 2.2.0 \
+     --app-version $VERSION \
      --strip-project-root \
      --minified-file /tmp/icbsorocaba.bundle \
      --minified-url main.jsbundle \
