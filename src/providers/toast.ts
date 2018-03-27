e5e65cc8569afbd5c64d82f94f2de9612a28ca03
Daniel Prado <danielprado.ad@gmail.com>
2018-03-27 10:22:28 -0300

    fix toast

--- src/providers/toast.ts
+++ src/providers/toast.ts
@@ -6,10 +6,6 @@ import { InteractionManager } from './interactionManager';
 type typeToast = 'danger' | 'success' | 'warning';
 
 export function toast(text: string, duration: number = 5000, type: typeToast = null): void {
-  // if (duration === 0) {
-  duration = 1000 * 60 * 60 * 60;
-  // }
-
   InteractionManager.runAfterInteractions(() => {
     Toast.show({
       text,
